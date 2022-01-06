9029c42a04982343e0f47d331a5f7683a28d2b7e
Rupinder Hayer <ruphayer@gmail.com>
2022-01-05 23:50:21 -0800

    added model for listing vehicles

---
+++ server/lib/models/listing.js
@@ -0,0 +1,33 @@
+const listingModel = (sequelize, DataTypes) =>
+  sequelize.define("listing", {
+    itemName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    condition: {
+      type: DataTypes.ENUM("new", "excellent", "good", "fair", "junk"),
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    obo: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+    },
+    barter: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+    },
+    createdBy: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
+module.exports = listingModel;
