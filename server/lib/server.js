8c94024ffd171a52976396aab6daedd576b58cc1
Rupinder Hayer <25388689+rupinderrr@users.noreply.github.com>
2022-03-11 00:10:00 -0800

    added server, middleware basic and bearer

--- server/lib/server.js
+++ server/lib/server.js
@@ -1,18 +1,18 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const cors = require('cors');
-
+const express = require("express");
+const cors = require("cors");
+const authRouter = require("./auth/routes.js");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(authRouter);
 
 module.exports = {
   app,
-  start: port => {
+  start: (port) => {
     app.listen(port, () => {
       console.log(`Server running on port ${port}`);
     });
