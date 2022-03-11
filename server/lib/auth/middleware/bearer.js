3e8e1db11b17fb5a84b9b4945bb87b7ddd9762c0
Roop Hayer <ruphayer@gmail.com>
2022-03-11 00:40:13 -0800

    added middleware basic and bearer auth

--- server/lib/auth/middleware/bearer.js
+++ server/lib/auth/middleware/bearer.js
@@ -1,23 +1,24 @@
-"use strict";
+'use strict';
 
-const { users } = require("../../models/index");
+const { users } = require('../../modelinstance/index.js');
 
 module.exports = async (req, res, next) => {
-  try {
-    if (!req.headers.authorization) {
-      _authError();
-    }
 
-    const token = req.headers.authorization.split(" ").pop();
+  try {
+
+    if (!req.headers.authorization) { _authError() }
+
+    const token = req.headers.authorization.split(' ').pop();
     const validUser = await users.authenticateToken(token);
     req.user = validUser;
     req.token = validUser.token;
     next();
+
   } catch (e) {
     _authError();
   }
 
   function _authError() {
-    next("Invalid Login");
+    next('Invalid Login');
   }
-};
+}
--- server/lib/server.js
+++ server/lib/server.js
@@ -1,18 +1,18 @@
-"use strict";
+'use strict';
 
-const express = require("express");
-const cors = require("cors");
-const authRouter = require("./auth/routes.js");
+const express = require('express');
+const cors = require('cors');
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(authRouter);
+
 
 module.exports = {
   app,
-  start: (port) => {
+  start: port => {
     app.listen(port, () => {
       console.log(`Server running on port ${port}`);
     });
